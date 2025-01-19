fe6c3127705a9922652a032ba55eeb9afddd2e66
Denner Bismarck <dennerbismarck@gmail.com>
2025-01-19 13:53:19 -0300

    Next - Receita 7

---
+++ recipes/react + nextjs/recipe 7/recipe_7/src/app/components/HeaderPage.js
@@ -0,0 +1,10 @@
+import { Header } from '../page';
+
+export default function HeaderPage() {
+    return (
+        <div>
+            <Header />
+            <p>Essa é a página do Header</p>
+        </div>
+    );
+}
---
+++ recipes/react + nextjs/recipe 7/recipe_7/src/app/novarota/teste/componentes.js
@@ -0,0 +1,6 @@
+export function Xablau() {
+  return <h2>Teste de várias rotas</h2>;
+}
+export function Mensagem({texto}){
+    return <h3>{texto}</h3>
+}
